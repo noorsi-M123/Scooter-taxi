c2dcac8f62866ba24cbd4777a2758dd5bc693693
noorsi-M123 <114653088+noorsi-M123@users.noreply.github.com>
2022-09-30 10:21:10 +0200

    app-config

--- backend/config.php
+++ backend/config.php
@@ -4,7 +4,7 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'acount');
 
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, 3308);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
